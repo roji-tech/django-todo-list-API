161d6c0d63abc86dd9f0611aa37c54c2861170e4
roji-tech <rojitech9@gmail.com>
2022-08-01 14:24:29 +0100

    added

--- todos/settings/prod.py
+++ todos/settings/prod.py
@@ -1,3 +1,4 @@
+from .common import *
 import os
 from .base import *
 import django_on_heroku
@@ -8,16 +9,69 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["todo-appl1.herokuapp.com"]
+ALLOWED_HOSTS = ["django-todolist-api.herokuapp.com"]
+
+
+DEBUG = os.environ.get("DEBUG", False)
+
+DEBUG = True
+
 
 DATABASES = {
     "default": dj_database_url.config()
 }
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-ALLOWED_HOSTS = ["todo-app-rjt.herokuapp.com"]
 
+# EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+# EMAIL_HOST_USER =os.environ['MAILGUN_SMTP_LOGIN']
+# EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+# EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configure Django App for Heroku.
 django_on_heroku.settings(locals())
+# https://peers-app-sh.herokuapp.com/
+
+# Debugging in heroku live
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "testlogger": {
+            "handlers": ["console"],
+            "level": "INFO",
+        }
+    },
+}
+
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+COMPRESS_ENABLED = os.environ.get("COMPRESS_ENABLED", False)
